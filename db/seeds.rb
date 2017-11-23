a9f562e6164b39e2631eaefa4fddda49ebd596e0
clement bacle <clement@officinasante.fr>
2017-11-23 14:35:26 +0100

    routes adding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "Absolut")
+Ingredient.create(name: "Polar Ice")
+Ingredient.create(name: "Danzka")
+Ingredient.create(name: "Frïs")
+Ingredient.create(name: "Grey Goose")
+Ingredient.create(name: "Jean-Marc XO")
+Ingredient.create(name: "Monte-Carlo")
+Ingredient.create(name: "Perfect 1864")
+Ingredient.create(name: "Russian Standard")
+Ingredient.create(name: "Stolichnaya")
+Ingredient.create(name: "Sobieski")
+Ingredient.create(name: "After Diner")
+Ingredient.create(name: "Café Rica")
+Ingredient.create(name: "Cubay")
+Ingredient.create(name: "Caffè Borghetti ")
+Ingredient.create(name: "Kahlua")
+Ingredient.create(name: "Kamok")
+Ingredient.create(name: "Kamora")
+Ingredient.create(name: "Marie Brizard")
+Ingredient.create(name: "Mokatika")
+Ingredient.create(name: "Monin")
+Ingredient.create(name: "lait")
+Ingredient.create(name: "Creme fraiche")
+
+
+
